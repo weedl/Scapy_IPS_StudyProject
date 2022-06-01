0dfc4ca6dba6f197819f1cfb11fc2193883eb9e8
weedle <94055286+weedl@users.noreply.github.com>
2022-06-01 22:03:23 +0200

    Update emailmessage.py

--- emailmessage.py
+++ emailmessage.py
@@ -5,14 +5,13 @@ EMAIL_ADDRESS = 'automated.mail@freelabs.com'
 EMAIL_PASSWORD = ''
 
 
-def sendmail():
-    global msg
+def sendmail(date):
     msg = EmailMessage()
     msg['Subject'] = 'Daily IPS report'
     msg['From'] = EMAIL_ADDRESS
     msg['To'] = 'head.of.security@freelabs.com'  # insert head of security's information here
     msg.set_content('Incident report')
-    msg.add_attachment(open('incident_report.txt', 'r').read())
+    msg.add_attachment(open('incident_report.txt' + date, 'r').read(), filaneme = 'incident_report.txt - ' + date)
 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp: # this has to change according to the mailing system being used in the task
         smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
