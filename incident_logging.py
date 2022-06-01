1d6305d7a7a77e1b57097860e5b162af0fd72952
weedle <94055286+weedl@users.noreply.github.com>
2022-06-01 22:24:19 +0200

    Update incident_logging.py

--- incident_logging.py
+++ incident_logging.py
@@ -1,20 +1,20 @@
 import shutil
 
 
-def new_log(date):
-    f = open('incident_report.txt' + date, 'w')
+def new_log():
+    f = open('incident_report', 'w')
     f.write('')
     f.close()
     # this block of code will wipe the file - making it ready for a new period of reporting
 
-def write(date):
-    f = open('incident_report.txt' + date, 'a')
+def write():
+    f = open('incident_report', 'a')
     f.write('This is an incident with code 404\n') # insert whatever shall be written to the file once an incident occurs here
     f.close()
 
 def cplog(date):
-    report = r'incident_report.txt' + date
-    log = r'path to log file' + 'log' + date
+    report = r'incident_report'
+    log = r'path to log directory' + report + '.' + date + '.txt'
     try:
         shutil.copyfile(report, log, follow_symlinks=True)
     except:
