30c23c8fbd3a48696687d889a0d3e62e728f37a3
weedle <94055286+weedl@users.noreply.github.com>
2022-06-01 21:07:34 +0200

    Update emailmessage.py

--- emailmessage.py
+++ emailmessage.py
@@ -1,27 +1,20 @@
 import smtplib
 from email.message import EmailMessage
-import imghdr
 
-from matplotlib.cbook import report_memory
-
-EMAIL_ADDRESS = 'automated.report@freelabs.com'
+EMAIL_ADDRESS = 'automated.mail@freelabs.com'
 EMAIL_PASSWORD = ''
 
+
 def sendmail():
-    global date
     global msg
     msg = EmailMessage()
     msg['Subject'] = 'Daily IPS report'
     msg['From'] = EMAIL_ADDRESS
-    msg['To'] = 'head.of.securitt@freelabs.com'  # insert head of security's information here
-
-    with open('incident_report.txt' + date, 'r') as f: # insert path to report file here
-        file_data = f.read()
-        file_type = imghdr.what(f.name)
-
-    msg.add_attachment(file_data)
+    msg['To'] = 'head.of.security@freelabs.com'  # insert head of security's information here
+    msg.set_content('Incident report')
+    msg.add_attachment(open('incident_report.txt', 'r').read())
 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp: # this has to change according to the mailing system being used in the task
         smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
 
-        smtp.send_message
+        smtp.send_message(msg)
