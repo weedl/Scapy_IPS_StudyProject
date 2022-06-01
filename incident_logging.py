f095a0f2b9eea555b8952f62da2308891fd89ccb
weedle <94055286+weedl@users.noreply.github.com>
2022-06-01 19:41:55 +0200

    Update incident_logging.py
    Added a copy log to location try except clause at the end of the cplog method

--- incident_logging.py
+++ incident_logging.py
@@ -1,21 +1,26 @@
 import os
 import shutil
+import subprocess
 
-def new_log():
-    f = open('incident_report.txt', 'w')
+def new_log(date):
+    f = open('incident_report.txt' + date, 'w')
     f.write('')
     f.close()
     # this block of code will wipe the file - making it ready for a new period of reporting
 
-def write():
-    f = open('incident_report.txt', 'a')
+def write(date):
+    f = open('incident_report.txt' + date, 'a')
     f.write('This is an incident with code 404\n') # insert whatever shall be written to the file once an incident occurs here
     f.close()
 
-def cplog():
-    report = r'incident_report.txt'
-    log = r'path to log file'
-
-    # this blcok of code is supposed to copy the content of the incident report txt file, and write it to a log directory. The log
-    # directory is supposed to be organized by date and time of the report. For instance the 08:00 report will be marked with:
-    # 08 - dd.mm.yyyy
+def cplog(date):
+    report = r'incident_report.txt' + date
+    log = r'path to log file' + 'log' + date
+    cmd = 'cp'
+    try:
+        copy_report = subprocess.Popen([cmd, report, log] stdout = subprocess.PIPE)
+        print(r'Report {} has been logged to the path: {}'.format(report, log)
+    except:
+        print('An error occured while trying to copy {} to destination')
+              
+              
